8cfc1a849392c66a06820414f7cf9a22ec51b5da
aliab <aliabid1571@gmail.com>
2026-04-28 20:50:11 +0500

    Flatten vercel config and fix template paths

--- bestcallproj/bestcallproj/settings.py
+++ bestcallproj/bestcallproj/settings.py
@@ -51,13 +51,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bestcallproj.urls'
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent # Points to the root BESTCALLS folder
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Matches your root templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -65,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 WSGI_APPLICATION = 'bestcallproj.wsgi.application'
 
@@ -111,15 +121,3 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-# settings.py
-
-STATIC_URL = 'static/'
-
-# Add this to tell Django where to find the 'static' folder in your root directory
-import os
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
