4dba2afea0989045d5663e52a07214219dfc262e
aliab <aliabid1571@gmail.com>
2026-04-28 21:05:14 +0500

    Update app paths to bestcallproj.bestcallapp

--- bestcallproj/bestcallapp/apps.py
+++ bestcallproj/bestcallapp/apps.py
@@ -1,5 +1,10 @@
+# bestcallproj/bestcallapp/apps.py
+
 from django.apps import AppConfig
 
 
 class BestcallappConfig(AppConfig):
-    name = 'bestcallapp'
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    # Update this name to match the full path
+    name = 'bestcallproj.bestcallapp'
