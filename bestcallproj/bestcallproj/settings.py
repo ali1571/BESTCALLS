8c1697204c70b27461b369f22fea79e62bd2bee1
aliab <aliabid1571@gmail.com>
2026-04-28 21:52:36 +0500

    liveeee

--- bestcallproj/bestcallproj/settings.py
+++ bestcallproj/bestcallproj/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- bestcallproj/bestcallproj/urls.py
+++ bestcallproj/bestcallproj/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bestcallproj.bestcallapp.urls')), # Make sure this points to your app
-]
+    path('', include('bestcallproj.bestcallapp.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
