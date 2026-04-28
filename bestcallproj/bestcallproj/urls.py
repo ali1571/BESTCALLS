9644faa7f7037ed6f033c66704e9abb0f491e9a0
aliab <aliabid1571@gmail.com>
2026-04-28 21:08:03 +0500

    Fix admin path syntax in urls.py

--- bestcallproj/bestcallproj/urls.py
+++ bestcallproj/bestcallproj/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.path),
+    path('admin/', admin.site.urls),
     path('', include('bestcallproj.bestcallapp.urls')), # Make sure this points to your app
 ]
